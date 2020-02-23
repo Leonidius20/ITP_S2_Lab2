57120ffdf58aecb64d50bc1606fb63c47433336d
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-02-23 18:57:33 +0200

    Implement getScholarshipReceivers

--- table.cpp
+++ table.cpp
@@ -1,8 +1,18 @@
 #include <vector>
-#include <string>
+#include <algorithm>
+#include <cmath>
 #include "student.h"
-#include "table.h"
+
+bool compareStudentsForScholarship(const Student &first, const Student &second);
 
 std::vector<Student> getScholarshipReceivers(const std::vector<Student> &students) {
-    return std::vector<Student>(); // TODO
+    std::vector<Student> receivers(students);
+    std::sort(receivers.begin(), receivers.end(), compareStudentsForScholarship);
+
+    int scholarshipCapacity = (int) std::round(receivers.size() * 0.4);
+    return std::vector<Student>(receivers.begin(), receivers.begin() + scholarshipCapacity);
+}
+
+bool compareStudentsForScholarship(const Student &first, const Student &second) {
+    return first.getAverageGrade() > second.getAverageGrade();
 }
