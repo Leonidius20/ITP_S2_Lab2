55e2b17bbe43af4f9ea9fe1947e52fc4911cc3ad
Leonidius20 <leonidslupsky2002@gmail.com>
2020-02-23 19:49:36 +0200

    refactoring

--- files.cpp
+++ files.cpp
@@ -12,6 +12,10 @@ using namespace std::filesystem;
 void processFile(const string& path, vector<Student>& students);
 void splitRecord(const string &str, string* words);
 
+const int NUMBER_OF_FIELDS = 7;
+const int NUMBER_OF_GRADES = 5;
+const int PRECISION = 3;
+
 vector<Student> processDirectory(const string& path) {
 	vector<Student> students;
 
@@ -25,7 +29,6 @@ vector<Student> processDirectory(const string& path) {
 }
 
 void processFile(const string& path, vector<Student>& students) {
-	setlocale(LC_ALL, "russian");
 	ifstream stream(path);
 	if (!stream.is_open()) {
 		cerr << "Failed to read file " << path;
@@ -33,25 +36,21 @@ void processFile(const string& path, vector<Student>& students) {
 	}
 	
 	string record;
-	getline(stream, record);
-	int records = stoi(record); // useless?
-	string elements[7];
+	getline(stream, record); // discarding number of students
+	string elements[NUMBER_OF_FIELDS];
 
 	while (getline(stream, record)) {
 		splitRecord(record, elements);
 		
 		double avgGrade = 0;
-		for (int i = 1; i < 6; i++) { // TODO: get rid of magic numbers
+		for (int i = 1; i < NUMBER_OF_FIELDS - 1; i++) {
 			avgGrade += stoi(elements[i]);
 		}
-		avgGrade /= 5;
+		avgGrade /= NUMBER_OF_GRADES;
 
-		Student s(elements[0], avgGrade);
-		cout << s.getLastName() << ", " << s.getAverageGrade() << endl; // for debug
-		students.push_back(s);
+		students.push_back(Student(elements[0], avgGrade));
 	}
 
-	cout << endl; // for debug
 	stream.close();
 }
 
@@ -64,7 +63,7 @@ void writeStudentsToFile(const std::vector<Student> &students, const std::string
 
 	for (Student student : students) {
 		stream << student.getLastName() << "," 
-			<< setprecision(3) << student.getAverageGrade() << endl;
+			<< setprecision(PRECISION) << student.getAverageGrade() << endl;
 	}
 
 	stream.close();
@@ -72,9 +71,9 @@ void writeStudentsToFile(const std::vector<Student> &students, const std::string
 
 void splitRecord(const string &str, string* words) {
 	int offset = 0; // a starting position from which to look for 
-					// the next occurence of 'delimiter'
-	int index;      // index of a 'delimiter' char in a string
-	for (int i = 0; i < 7; i++) {
+					// the next occurence of the delimiter
+	int index;      // index of the delimiter in the string
+	for (int i = 0; i < NUMBER_OF_FIELDS; i++) {
 		index = str.find(',', offset);
 		words[i] = str.substr(offset, (index - offset));
 		offset = index + 1;
